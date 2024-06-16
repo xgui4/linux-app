fc56ea010849772b0f9251a53be4e1900c843581
Xgui4 Studio <xgui2004@gmail.com>
2024-06-16 00:01:23 -0400

    initiate ! my git repo

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System; 
+  
+class program { 
+  
+    public static void Main(String[] args) 
+    { 
+        Console.WriteLine("My first terminal linux app writtent in c# !"); 
+    } 
+} 
